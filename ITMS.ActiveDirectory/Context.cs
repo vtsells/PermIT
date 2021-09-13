aae5f543cef512bdfa3c0d45ef9959c05dc663d2
Vincent Sells <tsells@bpc-financial.com>
2021-09-13 13:34:52 -0500

    Adding AD library

---
+++ ITMS.ActiveDirectory/Context.cs
@@ -0,0 +1,330 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMS.ActiveDirectory
+{
+    public class Context : IDisposable
+    {
+        private bool disposedValue;
+
+        // private PrincipalContext ADUserContext { get; }
+        private DirectoryContext ADContext { get; }
+
+        public Context(string domainName)
+        {
+            //ADUserContext = new PrincipalContext(ContextType.Domain, domainName);
+ 
+            ADContext = new DirectoryContext(DirectoryContextType.Forest,domainName);
+        }
+        #region Utilities
+        private DirectoryEntry GetADObject(string filter)
+        {
+            DirectoryEntry de = new DirectoryEntry(GetCurrentDomainPath());
+            var ds = new DirectorySearcher(de);
+            ds.Filter = filter;
+            return ds.FindOne()?.GetDirectoryEntry();
+        }
+        private SearchResultCollection GetADObjects(string filter)
+        {
+            DirectoryEntry de = new DirectoryEntry(GetCurrentDomainPath());
+
+            var ds = new DirectorySearcher(de);
+            ds.Filter = filter;
+            var all = ds.FindAll();
+            return all;
+        }
+        private string GetCurrentConfigurationDomainPath()
+        {
+          //  DirectoryEntry de = new DirectoryEntry("LDAP://RootDSE");
+            ////foreach(PropertyValueCollection prop in de.Properties)
+            ////{
+            ////    MessageBox.Show(prop.PropertyName+"_"+prop.Value);
+            ////}
+            ////  DirectoryEntry de = new DirectoryEntry("LDAP://configuration."+ADContext.Name);
+            ////  MessageBox.Show(de.Properties["configurationNamingContext"][0].ToString());
+            //   return "LDAP://" + de.Properties["configurationNamingContext"][0].ToString();
+            var name = "LDAP://CN=Configuration";
+            foreach (var s in ADContext.Name.Split('.'))
+            {
+                name += ",DC=" + s;
+            }
+          //  MessageBox.Show("LDAP://" + de.Properties["configurationNamingContext"][0].ToString() + "\n" + name);
+            return name;
+        }
+        private string GetCurrentDomainPath()
+        {
+            // DirectoryEntry de = new DirectoryEntry("LDAP://RootDSE");
+            //return "LDAP://" + de.Properties["defaultNamingContext"][0].ToString();
+            return "LDAP://" + ADContext.Name;
+        }
+        #endregion
+        #region Queries
+        public DirectoryEntry GetOneUser(string samName) =>
+            GetADObject( "(samAccountName=" + samName + ")");
+        public DirectoryEntry GetOneUser(string firstName, string lastName) =>
+            GetADObject("(&(givenName=" + firstName + ")(sn=" + lastName + "))");
+        public DirectoryEntry GetOneObjectBySID(string sid) =>
+            GetADObject("(objectSID=" + sid + ")");
+        public SearchResultCollection GetAllUsers() =>
+            GetADObjects("(sAMAccountType=805306368)");
+        public List<DirectoryEntry> GetAllMembersOf(string group)
+        {
+            DirectoryEntry de = new DirectoryEntry(GetCurrentDomainPath());
+            var ds = new DirectorySearcher(de);
+            ds.Filter = "(sAMAccountType=805306368)";
+            ds.PropertiesToLoad.Add("memberOf");
+            ds.PropertiesToLoad.Add("name");
+            var all = ds.FindAll();
+            var found = new List<DirectoryEntry>();
+            foreach(SearchResult result in all)
+            {
+                foreach(string prop in result.Properties["memberOf"])
+                {
+                    if (prop.Contains("CN="+group))
+                    {
+                        found.Add(result.GetDirectoryEntry());
+                        break;
+                    }
+                }
+            }
+            return found;
+        }
+        public SearchResultCollection GetAllComputers() =>
+            GetADObjects("(objectCategory=computer)");
+        public SearchResultCollection GetAllGroups() =>
+            GetADObjects("(objectCategory=group)");
+        public SearchResultCollection GetAllGroupsWMail() =>
+            GetADObjects("(&(objectCategory=group)"+"(mail=*))");
+        public SearchResultCollection GetWQuery(string query) =>
+            GetADObjects(query);
+        public ActiveDirectorySubnetCollection GetADSiteIPs(string siteName) =>
+            ActiveDirectorySite.FindByName(ADContext,siteName).Subnets;
+        public IEnumerable<string> GetOfficeLocations()
+        {
+            var users = GetAllUsers();
+            var locations = new List<string>();
+            foreach (SearchResult user in users)
+            {
+                var userLocation = user.GetDirectoryEntry().Properties[ADProperties.physicalDeliveryOfficeName.ToString()].Value?.ToString();
+                if (!locations.Any(m => m == userLocation) && userLocation?.Length>0)
+                {
+                    locations.Add(userLocation);
+                }
+               
+            }
+            return locations.OrderBy(m => m);
+        }
+        public SearchResultCollection GetAllPrinters() =>
+            GetADObjects("(objectCategory=printQueue)");
+        public List<ActiveDirectorySite> GetADSites()
+        {
+           // MessageBox.Show(GetCurrentConfigurationDomainPath());
+            DirectoryEntry de = new DirectoryEntry(GetCurrentConfigurationDomainPath());
+            var ds = new DirectorySearcher(de);
+            ds.Filter = "(objectClass=site)";
+            var sites = new List<ActiveDirectorySite>();
+            var results = ds.FindAll();
+            foreach (SearchResult sr in results)
+            {
+               // Using the index zero(0) is required!
+              //Console.WriteLine(sr.Properties["name"][0].ToString());
+              //  var subnets = ActiveDirectorySite.FindByName(ADContext, sr.Properties["name"][0].ToString()).Subnets;
+              //  if (subnets.Count > 0)
+              //  {
+              //      Console.WriteLine(subnets[0]);
+              //  }
+                sites.Add(ActiveDirectorySite.FindByName(ADContext, sr.Properties["name"][0].ToString()));
+            }
+            return sites;
+        }
+        #endregion
+        #region Edit Properties
+        public static string GetProperty(ADProperties property, SearchResult searchResult) =>
+            GetProperty(property, searchResult.GetDirectoryEntry());
+
+        public static string GetProperty(ADProperties property, DirectoryEntry entry) =>
+            (entry.Properties[property.ToString()].Value == null) ?
+            null : entry.Properties[property.ToString()].Value.ToString();
+        public static string GetProperty(string property, SearchResult searchResult) =>
+            GetProperty(property, searchResult.GetDirectoryEntry());
+
+        public static string GetProperty(string property, DirectoryEntry entry) =>
+            (entry.Properties[property.ToString()].Value == null) ?
+            null : entry.Properties[property.ToString()].Value.ToString();
+
+        public static void EditUserProperty(ADProperties property, string value, SearchResult searchResult) =>
+            EditUserProperty(property, value, searchResult.GetDirectoryEntry());
+        public static void EditUserProperty(ADProperties property, string value, DirectoryEntry entry)
+        {
+            entry.Properties[property.ToString()].Value = value;
+            entry.CommitChanges();
+        }
+        #endregion
+        //public SearchResultCollection GetAllComputers()
+        //{
+        //    DirectoryEntry de = new DirectoryEntry(GetCurrentDomainPath());
+        //    var ds = new DirectorySearcher(de);
+        //    ds.Asynchronous = true;
+        //    ds.Filter = "(objectClass=computer)";
+        //    //var computers= new List<DirectoryEntry>();
+
+        //    //var results = ds.FindAll();
+        //    ////foreach(var s in results[0].Properties.PropertyNames)
+        //    ////{
+        //    ////    Console.WriteLine(s);
+        //    //// }
+
+        //    //foreach (SearchResult sr in results)
+        //    //{
+        //    //   Console.WriteLine(sr.Properties[ComputerProperties.name.ToString()][0].ToString());
+
+        //    //    /*var buffer = sr.Properties[ComputerProperties.objectsid.ToString()][0] as Byte[];
+        //    //    var s = new SecurityIdentifier(buffer, 0);
+        //    //    Console.WriteLine(s.ToString() + "__");*/
+        //    //    computers.Add(sr.GetDirectoryEntry());
+
+        //    //}
+        //    return ds.FindAll();
+        //}
+
+        #region Enums
+        public enum ADProperties
+        {
+            /// <summary>
+            /// Account name
+            /// </summary>
+            samaccountname,
+            cn,
+            sn,
+            whencreated,
+            dnshostname,
+            lastlogon,
+            samaccounttype,
+            objectguid,
+            lastlogontimestamp,
+            operatingsystemversion,
+            name,
+            objectsid,
+            logoncount,
+            whenchanged,
+            objectcategory,
+            useraccountcontrol,
+            distinguishedname,
+            objectclass,
+            memberof,
+            adspath,
+            operatingsystem,
+            operatingsystemservicepack,
+            /// <summary>
+            /// User's manager
+            /// </summary>
+            manager,
+            /// <summary>
+            /// User's title
+            /// </summary>
+            title,
+            /// <summary>
+            /// User's phone number
+            /// </summary>
+            telephoneNumber,
+            /// <summary>
+            /// User's fax number
+            /// </summary>
+            facsimileTelephoneNumber,
+            /// <summary>
+            /// User's mobile number
+            /// </summary>
+            mobile,
+            /// <summary>
+            /// User's branch location
+            /// </summary>
+            physicalDeliveryOfficeName,
+            /// <summary>
+            /// User's P.O. Box
+            /// </summary>
+            postOfficeBox,
+            /// <summary>
+            /// User's street address
+            /// </summary>
+            streetAddress,
+            /// <summary>
+            /// User's city
+            /// </summary>
+            l,
+            /// <summary>
+            /// User's state
+            /// </summary>
+            st,
+            /// <summary>
+            /// User's zip code
+            /// </summary>
+            postalCode,
+            /// <summary>
+            /// User's country
+            /// </summary>
+            co,
+            /// <summary>
+            /// User's SID
+            /// </summary>
+            objectSID,
+            givenName,
+            initials,
+            description,
+            mail,
+            wWWHomePage,
+            userPrincipalName,
+            homeDirectory,
+            homeDrive,
+            scriptPath,
+            profilePath,
+            homePhone,
+            pager,
+            ipPhone,
+            info,
+            department,
+            company,
+            msExchRecipientTypeDetails
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~Context()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+}
---
+++ ITMS.ActiveDirectory/Extensions.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Net;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using static ITMS.ActiveDirectory.Context;
+
+namespace ITMS.ActiveDirectory
+{
+    public static class Extensions
+    {
+        public static string ToReadString(this Byte[] byteArray)
+        {
+            return  Encoding.UTF8.GetString(byteArray, 0, byteArray.Length);
+        }
+        public static string SID(this DirectoryEntry entry)
+        {
+            var buffer = entry.Properties[ADProperties.objectSID.ToString()][0] as Byte[];
+            return (new SecurityIdentifier(buffer, 0)).ToString();
+        }
+        public static string SID(this SearchResult searchResult)
+        {
+            return searchResult.GetDirectoryEntry().SID();
+        }
+        public async static Task<IPAddress[]> IP(string hostName)
+        {
+            var ips = await Dns.GetHostEntryAsync(hostName);
+            
+            return (ips==null)? null : ips.AddressList;
+        }
+        public async static Task<string> DNS(string ip)
+        {
+            var names = await Dns.GetHostEntryAsync(ip);
+            return (names == null) ? null : names.HostName;
+        }
+        public static bool Contains(this UserFlags haystack, UserFlags needle)
+        {
+            return (haystack & needle) == needle;
+        }
+        public static bool IsAccountDisabled(this DirectoryEntry user)
+        {
+            const string uac = "userAccountControl";
+            if (user.NativeGuid == null) return false;
+
+            if (user.Properties[uac] != null && user.Properties[uac].Value != null)
+            {
+                var userFlags = (UserFlags)user.Properties[uac].Value;
+                return userFlags.Contains(UserFlags.AccountDisabled);
+            }
+
+            return false;
+        }
+    }
+    [Flags]
+    public enum UserFlags
+    {
+        Script = 1,                                     // 0x1
+        AccountDisabled = 2,                            // 0x2
+        HomeDirectoryRequired = 8,                      // 0x8
+        AccountLockedOut = 16,                          // 0x10
+        PasswordNotRequired = 32,                       // 0x20
+        PasswordCannotChange = 64,                      // 0x40
+        EncryptedTextPasswordAllowed = 128,             // 0x80
+        TempDuplicateAccount = 256,                     // 0x100
+        NormalAccount = 512,                            // 0x200
+        InterDomainTrustAccount = 2048,                 // 0x800
+        WorkstationTrustAccount = 4096,                 // 0x1000
+        ServerTrustAccount = 8192,                      // 0x2000
+        PasswordDoesNotExpire = 65536,                  // 0x10000 (Also 66048 )
+        MnsLogonAccount = 131072,                       // 0x20000
+        SmartCardRequired = 262144,                     // 0x40000
+        TrustedForDelegation = 524288,                  // 0x80000
+        AccountNotDelegated = 1048576,                  // 0x100000
+        UseDesKeyOnly = 2097152,                        // 0x200000
+        DontRequirePreauth = 4194304,                   // 0x400000
+        PasswordExpired = 8388608,                      // 0x800000 (Applicable only in Window 2000 and Window Server 2003)
+        TrustedToAuthenticateForDelegation = 16777216,  // 0x1000000
+        NoAuthDataRequired = 33554432                   // 0x2000000
+    }
+}
